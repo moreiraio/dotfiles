c2567a7a0b65f902bd5686777c1681b3b85cbd09
vrmmoreira <vitor.moreira@gmail.com>
2023-05-22 12:28:00 +0100

    update

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,20 @@
+# Alias
+# ---
+#
+alias k="kubectl"
+alias h="helm"
+alias tf="terraform"
+alias a="ansible"
+alias ap="ansible-playbook"
+alias dt="datree"
+
+# mac OS shortcuts
+alias code="open -a 'Visual Studio Code'"
+
+
+
+# ALIAS COMMANDS
+alias ls="exa --icons --group-directories-first"
+alias ll="exa --icons --group-directories-first -l"
+alias g="goto"
+alias grep='grep --color'
